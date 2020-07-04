11b27e093013162444a7576342c3172ea06b06ad
Kévin Painchaud <mail@kpcd.me>
2020-07-04 10:24:23 +0200

    🚧 Main script - Removing exclude command construction

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh -l
 
-# Construct the exclude commands
-for excludedPath in "${INPUT_EXCLUDE[@]}"
-do
-  EXCLUDE="$EXCLUDE --exclude $excludedPath"
-done
-
 lftp $INPUT_FTP_HOST -u $INPUT_FTP_USERNAME,$INPUT_FTP_PASSWORD -e "$([ "$INPUT_DISABLE_SSL_CERTIFICATE_VERIFICATION" == true ] && echo "set ssl:verify-certificate false;") mirror --verbose --reverse $([ "$INPUT_DELETE" == true ] && echo "--delete") $INPUT_LOCAL_SOURCE_DIR $INPUT_DIST_TARGET_DIR; exit"
